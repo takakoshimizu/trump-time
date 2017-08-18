99fc00d3704ae7adbdcfe25f66cfc579946323ce
Erich Dalton <c.halcyon.days@gmail.com>
2017-08-18 14:22:54 -0400

    Added Bannons, months, and years.

--- src/cljs/trump_time/utils.cljs
+++ src/cljs/trump_time/utils.cljs
@@ -2,10 +2,13 @@
 
 
 (def scales
-  {:day     [1      "day"] 
+  {:day     [1      "day"]
+   :month   [30     "month"]
+   :year    [365    "year"]
    :mooch   [10     "mooch"] 
    :spicer  [183    "spicer"] 
    :priebus [182    "priebus"]
+   :bannon  [210.5  "bannon"]
    :comey   [109    "comey"]
    :manuf   [26.87  "manufacturing council shutdown"]
    :nazi    [2      "nazi condemnation turnaround"]
